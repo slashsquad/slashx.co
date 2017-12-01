2e8a1c409e990c8116748fbdf621611aa029606d
Moritz Lang <moritz@slashkeys.com>
2017-12-01 21:15:32 +0100

    Add project structure & configure webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const {resolve} = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const inProduction = (process.env.NODE_ENV === 'production')
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: './assets/css/app.css'
+})
 
 const config = {
   context: resolve(__dirname, 'src'),
@@ -6,8 +13,65 @@ const config = {
     app: './index.js'
   },
   output: {
-    filename: '[name].js',
+    filename: './assets/js/[name].js',
     path: resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.scss$/,
+        include: [resolve(__dirname, 'src', 'assets', 'sass')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !inProduction
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !inProduction
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/media/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: resolve(__dirname, './dist/assets/media'),
+    compress: true,
+    stats: 'errors-only',
+    port: 8888,
+    open: true
   }
 }
 
